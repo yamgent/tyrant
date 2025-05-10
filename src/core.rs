0f542c9b7d09c683b5a0fd83cb798c178180b68f
Maurice Wangleng Tan <tanwangleng@outlook.com>
2025-05-10 21:25:53 +0800

    feat: add dummy command bar

--- src/core.rs
+++ src/core.rs
@@ -5,30 +5,35 @@ use vello::{
 };
 
 use crate::{
+    command_bar::CommandBar,
     font::DefaultFonts,
     ui_text::{UiBasicText, UiFont},
 };
 
 pub struct Core {
     pub default_fonts: DefaultFonts,
+
+    pub command_bar: CommandBar,
 }
 
 impl Core {
     pub fn new() -> Self {
         Self {
             default_fonts: DefaultFonts::new(),
+            command_bar: CommandBar::new(),
         }
     }
 
     pub fn render(&self, scene: &mut Scene) {
-        let stroke = Stroke::new(6.0);
-        let rect = RoundedRect::new(10.0, 10.0, 240.0, 240.0, 20.0);
-        let rect_stroke_color = Color::new([0.9804, 0.702, 0.5294, 1.]);
-        scene.stroke(&stroke, Affine::IDENTITY, rect_stroke_color, None, &rect);
+        let mode = if self.command_bar.active() {
+            "COMMAND"
+        } else {
+            "NORMAL"
+        };
 
         if let Some(ui_font) = UiFont::new(self.default_fonts.monospace(), 32.0, &[]) {
             if let Some(mut ui_text) = UiBasicText::new(ui_font) {
-                ui_text.push_str("Hello world\nand bye!");
+                ui_text.push_str(mode);
                 ui_text.draw(
                     scene,
                     Affine::translate((40.0, 40.0)),
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod app;
+mod command_bar;
 mod core;
 mod font;
 mod oswin;
